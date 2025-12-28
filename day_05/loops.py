c28e7ffcc2a3217460817ff47b8f302b08cc4f33
Anvesh Singh <125560114+anvesh-s@users.noreply.github.com>
2025-12-29 02:30:59 +0530

    Implement for loop to print fruits and variants
    Add a for loop to iterate over fruits and print each fruit along with its pie variant.

---
+++ day_05/loops.py
@@ -0,0 +1,10 @@
+#for loop
+
+fruits = ['Apple','Peach','Pear']
+
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " pie")
+    print(fruits)
+    
+print(fruits) #no longer inside the loop and hence executes only when we are out of loop
