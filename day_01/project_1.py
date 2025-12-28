81d36a6e014e07179a103846bca2146ec4609e9d
Anvesh Singh <125560114+anvesh-s@users.noreply.github.com>
2025-12-28 23:24:47 +0530

    Add project_1.py to create band name from input

---
+++ day_01/project_1.py
@@ -0,0 +1,4 @@
+city_name = input("What is the name of the city you grew up in? ")
+pet_name = input("What is the name of your pet? ")
+
+print("the name of your band is: "+ city_name +" "+ pet_name)
