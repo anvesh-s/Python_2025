d6af6b9171b1b8192bd6333dff1c7ffd4a4f49a6
Anvesh Singh <125560114+anvesh-s@users.noreply.github.com>
2025-12-28 23:25:55 +0530

    Add basic Python data type examples and calculations

---
+++ day_02/hw.py
@@ -0,0 +1,54 @@
+#type error
+
+print("Hello"[0])
+print("Hello"[-1])
+print("123"+"345")
+
+#integer = whole number
+print(123+345)
+
+#float = floating point number
+print(22/7)
+
+#boolean
+print(True + 5)
+
+print(type("Hello"))
+print(type(True))
+print(type(123.45))
+
+print(int(123)+int (345))
+
+#value error
+
+print("Number of letters in your name: " + str(len(input("enter your name"))))
+
+print("my age is " + str(12))
+print(123+456)
+print(7-3)
+print(3*2)
+print(5/3)
+print(5//3)
+print(2**3)
+
+#PEMDAS LR
+print(3*3 + 3/3 -3)
+
+bmi = 84 / 1.65 **2
+print(bmi)
+print(int(bmi))
+print(round(bmi))
+print(round(bmi,2))
+
+score = 0
+#user scores a point
+score+=1
+print(score)
+
+#F STRINGS
+print("your score is " + str(23))
+
+print(f"your score is = {score}")
+
+print(int(2.7))
+print(round(2.7))
