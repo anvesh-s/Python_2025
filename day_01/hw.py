a8e704b3a950098c674199c6d80e8a28525bbcd2
Anvesh Singh <125560114+anvesh-s@users.noreply.github.com>
2025-12-28 23:24:11 +0530

    Add basic greeting program in hw.py
    Initial implementation of a simple greeting program that takes user input and prints various messages.

---
+++ day_01/hw.py
@@ -0,0 +1,20 @@
+print("hello world")
+
+name = input("whats your name? ")
+print("You're name is ",name)
+print("You're name is " + name)
+print(len(name))
+
+print("hello world \nhello world\nhello world")
+
+print("hello" + "angela")
+
+print ("hello" + " " "angela")
+
+#syntax error 
+#indentation error - spaces
+#name error
+
+
+
+
